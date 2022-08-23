c47a5ecf76fbe798eb136f788324230a8022dda3
C14F4 <68888946+C14F4@users.noreply.github.com>
2022-08-22 23:43:19 -0300

    Update ProyectosController.java

--- src/main/java/com/ciafa/portfolio/controller/ProyectosController.java
+++ src/main/java/com/ciafa/portfolio/controller/ProyectosController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 
 @RestController
 public class ProyectosController {
