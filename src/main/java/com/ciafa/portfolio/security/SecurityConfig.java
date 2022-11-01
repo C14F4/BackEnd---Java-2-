81b3cf2ec9c61dcdd76370f032a1f7536bc2690f
C14F4 <68888946+C14F4@users.noreply.github.com>
2022-10-31 22:22:54 -0300

    Update SecurityConfig.java

--- src/main/java/com/ciafa/portfolio/security/SecurityConfig.java
+++ src/main/java/com/ciafa/portfolio/security/SecurityConfig.java
@@ -15,16 +15,18 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+       /* Se comenta para poder probar el resto de la pagina por error de CORS en login
         http.csrf().disable().authorizeRequests()
             .antMatchers("/login").permitAll() 
             .anyRequest().authenticated() 
             .and()
-
+            // Las peticiones /login pasaran previamente por este filtro
             .addFilterBefore(new LoginFilter("/login", authenticationManager()),
                     UsernamePasswordAuthenticationFilter.class)
-
+            // Las demás peticiones pasarán por este filtro para validar el token
             .addFilterBefore(new JwtFilter(),
                     UsernamePasswordAuthenticationFilter.class);
+        */
     }
 
     @Override
